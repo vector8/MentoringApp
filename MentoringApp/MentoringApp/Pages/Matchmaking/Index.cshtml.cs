aaf16e18f88affb5512b71030648d512a655976a
vector8 <vector8@users.noreply.github.com>
2018-03-28 12:07:21 -0400

    Added broken Matchmaking index page.

---
+++ MentoringApp/MentoringApp/Pages/Matchmaking/Index.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MentoringApp.Data;
+using MentoringApp.Models;
+
+namespace MentoringApp.Pages.Matchmaking
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MentoringApp.Data.ApplicationDbContext _context;
+
+        public IndexModel(MentoringApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+
+            var values = Enum.GetValues(typeof(Question.QuestionType)).Cast<Question.QuestionType>();
+
+            QuestionTypeText = new String[values.Count()];
+
+            for(int i = 0; i < values.Count(); i++)
+            {
+                QuestionTypeText[i] = Question.GetDescription(values.ElementAt(i));
+            }
+        }
+
+        public IList<Question> question { get;set; }
+
+        public String[] QuestionTypeText { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            question = await _context.Question.ToListAsync();
+        }
+    }
+}
